e358a303b550abb6161d773eb8834d546b5c9f2e
DarioPazmino <dariopb1497@gmail.com>
2022-05-16 13:20:53 -0500

    added stripe payments and components

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,24 +1,30 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles.jsx';
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+  ButtonSpinner,
+} from "./button.styles.jsx";
 
 export const BUTTON_TYPES_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted'
+  base: "base",
+  google: "google-sign-in",
+  inverted: "inverted",
 };
 
-const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
-  {
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
+  ({
     [BUTTON_TYPES_CLASSES.base]: BaseButton,
     [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
-    [BUTTON_TYPES_CLASSES.inverted]: InvertedButton
-  }[buttonType]
-)
+    [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButtom = getButton(buttonType);
   return (
-    <CustomButtom {...otherProps}>{children}</CustomButtom>
+    <CustomButtom disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButtom>
   );
 };
 
-export default Button
+export default Button;
--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { SpinnerContainer } from '../spinner/spinner.styles';
+
 export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
@@ -18,6 +20,7 @@ export const BaseButton = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
+  align-items: center;
   transition: .3s ease-in-out;
 
   &:hover {
@@ -47,4 +50,10 @@ export const InvertedButton = styled(BaseButton)`
     background-color: #E5C9A5;
     border: none;
   }
+`;
+
+
+export const ButtonSpinner = styled(SpinnerContainer)`
+  width: 30px;
+  height: 30px;
 `;
